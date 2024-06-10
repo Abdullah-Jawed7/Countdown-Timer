86a2f246fd959ea4fc84f91e62ab241b5feafb47
One-andOnly <162917702+One-andOnly@users.noreply.github.com>
2024-06-10 22:05:11 +0500

    CountDown

---
+++ timer.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+async function aaa() {
+  let time = await inquirer.prompt({
+    name: "time",
+    type: "number",
+    message: chalk.yellow("Enter a seconds you want to start CountDown :"),
+  });
+
+  let count = time.time + 1;
+  let start = setInterval(function () {
+    count--;
+    console.log(chalk.cyan(`Count Down : ${count}`));
+    if (count == 0) {
+      console.log(chalk.yellow("Your CountDown has Completed"));
+
+      clearInterval(start);
+    }
+  }, 1000);
+}
+aaa();
